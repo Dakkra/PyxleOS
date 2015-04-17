3d188dfa996ebb94a9cfd7cd0b5c2e5fa7a74507
Chris Soderquist <contact@dakkra.com>
2015-04-17 17:33:06 -0600

    Removed redundant //TODO

--- PyxleOS/src/main/java/com/dakkra/pyxleos/view/MainView.java
+++ PyxleOS/src/main/java/com/dakkra/pyxleos/view/MainView.java
@@ -50,7 +50,7 @@ public class MainView {
 		m.mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		m.mainFrame.setMinimumSize(new Dimension(800,600));
 		m.mainFrame.setLocationRelativeTo(null);
-		m.mainFrame.setJMenuBar(m.menuBar);//TODO make menu bar
+		m.mainFrame.setJMenuBar(m.menuBar);
 		
 		// Icon code
 		Image icon;
