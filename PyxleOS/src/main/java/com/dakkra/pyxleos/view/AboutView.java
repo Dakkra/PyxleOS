a28aff4eba2f2f51dacfff839c2885fde8392559
Chris Soderquist <contact@dakkra.com>
2015-04-17 20:01:04 -0600

    made the AboutView much nicer to use

--- PyxleOS/src/main/java/com/dakkra/pyxleos/view/AboutView.java
+++ PyxleOS/src/main/java/com/dakkra/pyxleos/view/AboutView.java
@@ -1,17 +1,22 @@
 package com.dakkra.pyxleos.view;
 
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.Image;
 import java.io.IOException;
 import java.io.InputStream;
 
 import javax.imageio.ImageIO;
 import javax.swing.JFrame;
+import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.UIManager;
 import javax.swing.UIManager.LookAndFeelInfo;
 
+import com.dakkra.pyxleos.model.MainModel;
+
 public class AboutView {
+	public MainModel m = new MainModel();
 	public void showAboutView(){
 		//use ninbus
 		for (LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
@@ -28,9 +33,10 @@ public class AboutView {
 		}
 		
 		JFrame aboutViewFrame = new JFrame("About PyxleOS");
-		aboutViewFrame.setSize(350, 170);
+		aboutViewFrame.setMinimumSize(new Dimension(390,170));
+		aboutViewFrame.setSize(390, 170);
 		aboutViewFrame.setLocationRelativeTo(null);
-		aboutViewFrame.setResizable(false);
+		aboutViewFrame.setResizable(true);
 		aboutViewFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		
 		// Icon code
@@ -44,11 +50,12 @@ public class AboutView {
 			// Intentionally ignore exception (because it should never happen)
 		}
 		
-		JTextArea about = new JTextArea("(c) By Chris Soderquist\n http://pyxleos.sourceforge.net \n \nPyxleOS is under a GNU GENERAL PUBLIC LICENSE V2.0\n https://www.gnu.org/licenses/gpl-2.0.html \n\n Check out http://dakkra.com \n Name: Pyxle(pixel) OS(open source)");
-		about.setEditable(false);
-		about.setLineWrap(true);
+		JTextArea aboutText = new JTextArea(m.applicationNameVersion+"\n(c) By Chris Soderquist\n http://pyxleos.sourceforge.net \n \nPyxleOS is under a GNU GENERAL PUBLIC LICENSE V2.0\n https://www.gnu.org/licenses/gpl-2.0.html \n\nPyxle(pixel) OS(open source)\n\nCheck out http://dakkra.com");
+		aboutText.setEditable(false);
+		aboutText.setLineWrap(true);
+		JScrollPane aboutTextPane = new JScrollPane(aboutText);
 		
-		aboutViewFrame.add(about);
+		aboutViewFrame.add(aboutTextPane);
 		
 		aboutViewFrame.setVisible(true);
 		
