e564cf100c61dd358efb033cdb2d1c05b1761c2a
dakkra <amazon@dakkra.com>
2015-05-10 14:10:12 -0600

    Changed starting string

--- src/main/java/com/dakkra/pyxleos/Main.java
+++ src/main/java/com/dakkra/pyxleos/Main.java
@@ -13,7 +13,7 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		System.out.println("Starting up...");
+		System.out.println("Starting...");
 
 		MainView v = new MainView();
 		MainModel m = new MainModel();
