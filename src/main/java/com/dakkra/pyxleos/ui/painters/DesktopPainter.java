e3252e22953013c1835db35db0d533f169b7e350
dakkra <amazon@dakkra.com>
2015-05-26 20:44:28 -0600

    Catchin up and cleaning up

--- src/main/java/com/dakkra/pyxleos/ui/painters/DesktopPainter.java
+++ src/main/java/com/dakkra/pyxleos/ui/painters/DesktopPainter.java
@@ -3,13 +3,10 @@ package com.dakkra.pyxleos.ui.painters;
 import java.awt.Graphics2D;
 import java.awt.Image;
 import java.awt.RenderingHints;
-import java.io.IOException;
 
-import javax.imageio.ImageIO;
 import javax.swing.JDesktopPane;
 import javax.swing.Painter;
 
-import com.dakkra.pyxleos.PyxleOS;
 import com.dakkra.pyxleos.ui.UISettings;
 
 public class DesktopPainter implements Painter<JDesktopPane> {
