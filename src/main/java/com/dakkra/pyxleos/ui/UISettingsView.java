bfeab918b34e2e09ab7314c173402ff529e889f1
dakkra <amazon@dakkra.com>
2015-06-05 13:52:44 -0600

    Continued setup for appearance manager

--- src/main/java/com/dakkra/pyxleos/ui/UISettingsView.java
+++ src/main/java/com/dakkra/pyxleos/ui/UISettingsView.java
@@ -4,6 +4,7 @@ import java.awt.FlowLayout;
 
 import javax.swing.JButton;
 import javax.swing.JInternalFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 import com.dakkra.pyxleos.util.Util;
@@ -21,18 +22,33 @@ public class UISettingsView {
 		
 		FlowLayout layout = new FlowLayout();
 		layout.setAlignment(FlowLayout.LEFT);
+		
+		JPanel bgColorPanel = new JPanel();
+		bgColorPanel.setLayout(layout);
+		bgColorPanel.add(new JLabel("BG Color: "));
+		bgColorPanel.add(new JButton("Color"));
 
-		JPanel controlColorPanel = new JPanel();
+		JPanel baseColorPanel = new JPanel();
+		baseColorPanel.setLayout(layout);
+		baseColorPanel.add(new JLabel("Base Color: "));
+		baseColorPanel.add(new JButton("Color"));
+
 		
-		controlColorPanel.setLayout(layout);
-		
-		controlColorPanel.add(new JButton("LOL1"));
-		controlColorPanel.add(new JButton("LOL2"));
-		controlColorPanel.add(new JButton("LOL3"));
-		
+		JPanel baseRedColorPanel = new JPanel();
+		baseRedColorPanel.setLayout(layout);
+		baseRedColorPanel.add(new JLabel("Red Color: "));
+		baseRedColorPanel.add(new JButton("Color"));
+
+		JPanel textColorPanel = new JPanel();
+		textColorPanel.setLayout(layout);
+		textColorPanel.add(new JLabel("TextColor: "));
+		textColorPanel.add(new JButton("Color"));
+
 		frame.setLayout(layout);
-		
-		frame.add(controlColorPanel);
+		frame.add(bgColorPanel);
+		frame.add(baseColorPanel);
+		frame.add(baseRedColorPanel);
+		frame.add(textColorPanel);
 		
 		mw.addIFrame(frame);
 	}
