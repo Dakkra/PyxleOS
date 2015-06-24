000f53429f2fca11b5a506ea37749da6e614bfcf
Chris Soderquist <contact@dakkra.com>
2015-06-24 16:19:07 -0600

    moe newline conversion

--- src/main/java/com/dakkra/pyxleos/ui/MainWindow.java
+++ src/main/java/com/dakkra/pyxleos/ui/MainWindow.java
@@ -245,7 +245,6 @@ public class MainWindow {
 	public void updateNimbus() {
 		Color bgColor = uis.getbgColor();
 		Color baseColor = uis.getbaseColor();
-		System.out.println(baseColor);
 		Color baseRedColor = uis.getselectionRedColor();
 		Color textColor = uis.gettextColor();
 
